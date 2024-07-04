05e8e8147a6c4fdd8d690ea24c3a16ef2e185eff
marianobaroni123456 <marianobaroni123456@gmail.com>
2024-07-04 02:36:11 -0300

    falto de agregar

--- BlazorAppVSCode/Program.cs
+++ BlazorAppVSCode/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAppVSCode;
+using BlazorAppVSCode.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 var urlApi = builder.Configuration.GetValue<string>("urlApi");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(urlApi) });
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(urlApi) });
+builder.Services.AddScoped<IAlumnoServices, AlumnoService>();
 await builder.Build().RunAsync();
---
+++ BlazorAppVSCode/Services/AlumnoService.cs
@@ -0,0 +1,27 @@
+﻿using BlazorAppVSCode.Models;
+using System.Text.Json;
+
+namespace BlazorAppVSCode.Services
+{
+    public class AlumnoService : IAlumnoServices
+    {
+        private readonly HttpClient client;
+        //private readonly JsonSerializerOptions options;
+
+        public AlumnoService(HttpClient client, JsonSerializerOptions options)
+        {
+            this.client = client;
+           // this.options = options;
+        }
+        public async Task<List<Alumno>?> Get()
+            {
+                var response = await client.GetAsync("apialumnos");
+                return await JsonSerializer.DeserializeAsync<List<Alumno>>(await response.Content.ReadAsStreamAsync());
+
+            }
+    }
+    public interface IAlumnoServices
+    {
+        public Task<List<Alumno>?> Get();
+    }
+}
